334b2d23e5afbbc24b845e9e7b941ac4fc418e49
Yacnnn <73817884+Yacnnn@users.noreply.github.com>
2020-12-02 12:40:40 +0100

    Update run_algebraic_methods.py

--- run_algebraic_methods.py
+++ run_algebraic_methods.py
@@ -10,13 +10,16 @@ from tqdm import tqdm
 from sklearn.model_selection import GridSearchCV
 
 def available_tasks():
-    return ["uci7",'uci10']
+    return ["uci7",'uci10','tfr']
 
 def available_methods():
     return ["pca",'gpca','mcca','gmcca']
 
-def evaluate_clustering_gpca(X, labels, nb_clusters = 7, latent_dim = 3, beta_list = [0], nb_run = 1):
-    Xconcat = np.concatenate(X,axis = 1)
+def evaluate_clustering_gpca(data, latent_dim = 3, beta_list = [0], nb_run = 1):
+    Xconcat = np.concatenate(data["X"],axis = 1)
+    W = data["W"]
+    labels = data["labels"]
+    nb_clusters = data["nb_clusters"]
     Xpca_tab = []
     labels = np.array(labels)
     for beta in tqdm( beta_list, unit = 'beta' ):
@@ -24,13 +27,37 @@ def evaluate_clustering_gpca(X, labels, nb_clusters = 7, latent_dim = 3, beta_li
         Xpca_tab.append(Xpca)
     process_evaluation.evaluate_clustering_cross_validation(Xpca_tab, labels, nb_clusters = nb_clusters, nb_run = nb_run)
 
-def evaluate_clustering_gmcca(X, labels, nb_clusters = 7, latent_dim = 3, gamma_list = [0], nb_run = 1):
+def evaluate_clustering_gmcca(data, latent_dim = 3, gamma_list = [0], nb_run = 1):
+    X = np.concatenate(data["X"],axis = 1)
+    W = data["W"]
+    labels = data["labels"]
+    nb_clusters = data["nb_clusters"]
     Stmcca_tab = []
     labels = np.array(labels)
     for gamma in tqdm(gamma_list):
         Stmcca, _, _ = algebraic.gmmca(X,W,gamma,n_components = latent_dim)  
         Stmcca_tab.append(Stmcca)
     process_evaluation.evaluate_clustering_cross_validation(Stmcca_tab, labels, nb_clusters = nb_clusters, nb_run = nb_run)
+
+def evaluate_friendrecommandation_gpca(data, latent_dim = 3, beta_list = [0], nb_run = 1):
+
+
+def evaluate_uci(method):
+    if method == "pca":
+        beta_list = [0]
+        evaluate_clustering_gpca(data, latent_dim = 3, beta_list = beta_list, nb_run = 10)
+    if method == "gpca":
+        beta_list = list(np.logspace(-2,0,20))
+        evaluate_clustering_gpca(data, latent_dim = 3, beta_list = beta_list, nb_run = 100)
+    elif method == "mcca":
+        gamma_list = [0]
+        evaluate_clustering_gmcca(data, latent_dim = 3, gamma_list = gamma_list, nb_run = 10)
+    elif method == "gmcca":
+        gamma_list = list(np.logspace(-2,0,20))
+        evaluate_clustering_gmcca(data, latent_dim = 3, gamma_list = gamma_list, nb_run = 100)
+
+def evaluate_tfr(method):
+    print("TO DO")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -39,25 +66,12 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.task in available_tasks():
-        X, W = process_data.load_dataset(args.task)
-        if args.task == "uci7":
-            nb_clusters = 7
-        elif args.task == "uci10":
-            nb_clusters = 10
-        labels = process_data.get_uci_labels(nb_clusters=nb_clusters)
+        data = process_data.load_dataset(args.task)
         if args.method in available_methods():
-            if args.method == "pca":
-                beta_list = [0]
-                evaluate_clustering_gpca(X, labels, nb_clusters = nb_clusters, latent_dim = 3, beta_list = beta_list, nb_run = 10)
-            if args.method == "gpca":
-                beta_list = list(np.logspace(-2,0,20))
-                evaluate_clustering_gpca(X, labels, nb_clusters = nb_clusters, latent_dim = 3, beta_list = beta_list, nb_run = 100)
-            elif args.method == "mcca":
-                gamma_list = [0]
-                evaluate_clustering_gmcca(X, labels, nb_clusters = nb_clusters, latent_dim = 3, gamma_list = gamma_list, nb_run = 10)
-            elif args.method == "gmcca":
-                gamma_list = list(np.logspace(-2,0,20))
-                evaluate_clustering_gmcca(X, labels, nb_clusters = nb_clusters, latent_dim = 3, gamma_list = gamma_list, nb_run = 100)
+            if "uci" in args.task:
+                evaluate_uci(args.method)
+            elif "tfr" in args.task:
+                evaluate_tfr(args.method)
         else:
             print('Unknown method %s'%args.method)
             parser.print_help()
