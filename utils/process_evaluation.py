ddd3cbf9e171a047f0b42886adc2d01e072816e5
Yacnnn <73817884+Yacnnn@users.noreply.github.com>
2021-06-03 13:06:19 +0200

    Update process_evaluation.py

--- utils/process_evaluation.py
+++ utils/process_evaluation.py
@@ -17,6 +17,8 @@ from grakel.kernels import PropagationAttr
 
 from sklearn.model_selection import train_test_split
 
+from scipy.spatial.distance import cosine
+
 
 def svm_fit_rbf(ZTrain,labelsTrain,ZTest,labelsTest,nbclass = 2 ):
     clf = SVC(gamma='auto',kernel="rbf", C = 1)
@@ -29,19 +31,16 @@ def evaluate_clustering(Zo, labels, nb_clusters = 7, display_score = True, displ
     scaler = StandardScaler()
     scaler.fit(Z)
     Z = scaler.transform(Z)
-    
     kmeans_ = KMeans(n_clusters=nb_clusters, random_state=0).fit(Z)
     kmeans_clustering_score = adjusted_rand_score(kmeans_.labels_,labels)
     spectral_ = SpectralClustering(n_clusters=nb_clusters,assign_labels="discretize", random_state=0, gamma=5).fit(Z)
     spectral_clustering_score = adjusted_rand_score(spectral_.labels_,labels)
     clf = SVC(kernel = "rbf")
     svm_accuracy = np.mean(cross_val_score(clf, Z, np.array(labels), cv=5))
-    
     if display_score :
         print("Kmeans Clustering Score "+display_name+" on UCI: "+str(kmeans_clustering_score))
         print("Spectral Clustering Score "+display_name+" on UCI: "+str(spectral_clustering_score))
         print("Svm-rbf Accuracy "+display_name+" on UCI: "+str(svm_accuracy))
-        
     if display_graph_with_tsne :
         tsne = TSNE(perplexity=30, n_components=2, init='pca', n_iter=3000)
         Z_tsne = tsne.fit_transform(Z)
@@ -54,7 +53,6 @@ def evaluate_clustering(Zo, labels, nb_clusters = 7, display_score = True, displ
         plt.title(display_name)
         plt.scatter(Z_tsne[:, 0],  Z_tsne[:, 1], s=10,c=color_list);
         plt.show()
-
     if write_score :
         sio.savemat(write_path+"clustering_results.mat",mdict = {
             "Kmeans Clustering Score" : kmeans_clustering_score,
@@ -63,77 +61,63 @@ def evaluate_clustering(Zo, labels, nb_clusters = 7, display_score = True, displ
         })
     return [kmeans_clustering_score, spectral_clustering_score, svm_accuracy]
 
-def evaluate_clustering_cross_validation(Zo_tab, labels, nb_clusters = 7, nb_run = 10, display_score = True, display_name = ''):#, display_graph_with_tsne = True, display_name = "Title",  write_score = False, write_path = ''):
-
-    svm_accuracy_acc =  0
-    clustering_score_acc = 0
-    spect_clustering_score_acc = 0
-    labels_original = labels
-    for run in range(nb_run):
-        labels = labels_original
-        num_of_instance = Zo_tab[0].shape[0]
-        s = np.arange(num_of_instance)
-        np.random.shuffle(s)
-
-        Z_tab = []
-        for Z in Zo_tab:
-            scaler = StandardScaler()
-            scaler.fit(Z)
-            Z_tab.append(scaler.transform(Z)[s,:])
-        tuned_parameters = {'kernel': ['rbf'], 
-                            'gamma': ["auto"]+list(np.logspace(-3,3,7)),
-                                'C': [1]+list(np.logspace(-3,3,7))}
-
-        train_cut = int(0.9*num_of_instance)
-
-        labels = labels[s]
-        labelsTrain = labels[train_cut:]
-        labelsTest = labels[:train_cut]
-
-        ZTrain_tab = [Z[train_cut:] for Z in Z_tab]#Z_tab
-        ZTest_tab = [Z[:train_cut] for Z in Z_tab]#Z_tab
-
-        svm_acc_tab = []
-        parameters_tab = []
-        for Z in ZTrain_tab:
-            clf = GridSearchCV( SVC(), tuned_parameters, refit = True, scoring='accuracy',)
-            clf.fit(Z, labelsTrain)
-            svm_acc_tab.append(clf.best_score_)
-            parameters_tab.append(clf.best_params_)
-
-        ibest = np.argmax(svm_acc_tab)
-        best_parameters = parameters_tab[ibest]
-
-        bestbeta_ZTrain = ZTrain_tab[ibest]
-        bestbeta_ZTest = ZTest_tab[ibest]
-        bestbeta_Z = Z_tab[ibest]
-
-        final_clf =  SVC(gamma = best_parameters["gamma"], kernel = best_parameters["kernel"], C = best_parameters["C"])
-        final_clf.fit(bestbeta_ZTrain,labelsTrain)
-        svm_accuracy = accuracy_score(final_clf.predict(bestbeta_ZTest),labelsTest)
-
-        # final_clf =  SVC(gamma = best_parameters["gamma"], kernel = best_parameters["kernel"], C = best_parameters["C"])
-        # svm_accuracy = np.mean(cross_val_score(final_clf, bestbeta_Z,labels, cv=10))
-
-        kmeansgpca = KMeans(n_clusters=nb_clusters, random_state=0).fit(bestbeta_Z)
-        clustering_score = adjusted_rand_score(kmeansgpca.labels_,labels)
-
-        spectral = SpectralClustering(n_clusters=nb_clusters,assign_labels="discretize", random_state=0, gamma=5).fit(bestbeta_Z)
-        spect_clustering_score = adjusted_rand_score(spectral.labels_,labels)
-
-        # if display_score :
-        #     print("Svm-rbf Accuracy "+display_name+" on UCI: "+str(svm_accuracy))
-        #     print("Clustering Score "+display_name+" on UCI: "+str(clustering_score))
-        #     print("Spec Clustering Score "+display_name+" on UCI: "+str(spect_clustering_score))
-        #     print("best_parameters "+display_name+" on UCI: "+str(best_parameters))
-
-        svm_accuracy_acc +=  svm_accuracy/nb_run
-        clustering_score_acc += clustering_score/nb_run
-        spect_clustering_score_acc += spect_clustering_score/nb_run
-
+def evaluate_friend_recommendation(X, X_id, dict_hashtag_users_, display_score = True, display_name =  "Method", ref_size = 5,  l_value = 35):
+    dict_hashtag_users = dict_hashtag_users_.copy()
+    scaler = StandardScaler()
+    scaler.fit(X)
+    X = scaler.transform(X)   
+    users_ref = []
+    users_ref2 = []
+    for key in dict_hashtag_users.keys():
+        np.random.shuffle(dict_hashtag_users[key])
+        users_ref.append(dict_hashtag_users[key][:ref_size])
+        users_ref2 = users_ref2 + list(dict_hashtag_users[key][:ref_size])
+        dict_hashtag_users[key] = dict_hashtag_users[key][ref_size:]
+    # Xnew = []
+    # Xnew_id = []
+    # for k in range(len(X_id)) : 
+    #     if not X_id[k] in users_ref2:
+    #         Xnew.append(X[k,:])
+    #         Xnew_id.append(X_id[k])
+    Xref = []
+    for tab in users_ref:
+        tampon = []
+        for id_ in tab :
+            index = np.where(id_ == X_id)[0][0]
+            tampon.append(X[index,:])
+        Xref.append(np.mean(tampon,axis=0))
+    distances = []
+    for ref in Xref:
+        distance = []
+        for k in range(len(X)):
+            distance.append(cosine(ref,X[k][:]))
+        distances.append(distance)
+    distances_order = [np.argsort(distance) for distance in distances]
+    k = 0
+    dict_classif = {}
+    for key in dict_hashtag_users.keys():
+        dict_classif.update({key : []})
+        for l in distances_order[k]:
+            dict_classif[key].append(X_id[l])
+        k = k + 1
+    L = l_value
+    precision = 0
+    recall = 0
+    mrr = 0
+    integer = 0
+    for key in dict_hashtag_users.keys() :
+        if not len(dict_hashtag_users[key]) == 0 :
+            precision += len(np.intersect1d(dict_hashtag_users_[key],dict_classif[key][:L]))/L
+            recall += len(np.intersect1d(dict_hashtag_users_[key],dict_classif[key][:L]))/len(dict_hashtag_users_[key])
+            _, _, first_indices =  np.intersect1d(dict_hashtag_users_[key],dict_classif[key],return_indices = True)
+            if len(first_indices) > 0 :
+                mrr += 1./(min(first_indices)+1)#[0]
+            integer += 1
+    precision = precision/float(integer)
+    recall = recall/float(integer)
+    mrr = mrr/integer
     if display_score :
-        print("Svm-rbf Accuracy "+display_name+" on UCI: "+str(svm_accuracy_acc))
-        print("Clustering Score "+display_name+" on UCI: "+str(clustering_score_acc))
-        print("Spec Clustering Score "+display_name+" on UCI: "+str(spect_clustering_score_acc))
-
-    return svm_accuracy_acc, clustering_score_acc, spect_clustering_score_acc
+        print("Precison "+display_name+" on TEP: "+str(precision))
+        print("Recall "+display_name+" on TEP: "+str(recall))
+        print("Mrr "+display_name+" on TEP: "+str(mrr))
+    return precision, recall, mrr
